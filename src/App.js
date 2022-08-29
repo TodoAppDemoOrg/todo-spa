cbadcd9d1d4ec1cd39f8d5563b77ff68bca96e13
mofr <mofr@zond.org>
2022-08-29 15:40:59 +0200

    Extract TodoAppBar from App

--- src/App.js
+++ src/App.js
@@ -1,67 +1,13 @@
 import * as React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { styled, alpha } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import SearchIcon from '@mui/icons-material/Search';
-import InputBase from '@mui/material/InputBase';
-import DoneOutlineIcon from '@mui/icons-material/DoneOutline';
 import TodoList from './TodoList';
 import TodoDialog from './TodoDialog';
 import { CircularProgress } from '@mui/material';
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
+import TodoAppBar from './TodoAppBar';
 
 function App() {
-  const openCreateTodoDialog = () => {
-    console.log("openCreateTodoDialog");
-  };
-
   async function getTodos() {
     let response = await axios("https://jsonplaceholder.typicode.com/todos/");
     return response.data;
@@ -71,50 +17,7 @@ function App() {
 
   return (
     <div>
-    <AppBar position="static">
-      <Container>
-      <Toolbar disableGutters>
-          <DoneOutlineIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            TODOAPP
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            <Button
-              onClick={openCreateTodoDialog}
-              sx={{ my: 2, color: 'white', display: 'block' }}
-            >
-              {"Create Task"}
-            </Button>
-          </Box>
-
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-
-          </Toolbar>
-      </Container>
-    </AppBar>
+    <TodoAppBar/>
     <Container>
       {query.isFetched ? <TodoList todos={query.data}/> : <CircularProgress/>}
       <TodoDialog/>
