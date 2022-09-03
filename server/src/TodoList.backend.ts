54fc04e64a5074f88c82b580d6ecc603eaa2708f
mofr <mofr@zond.org>
2022-09-03 15:14:27 +0200

    Minor code clean up

--- server/src/TodoList.backend.ts
+++ server/src/TodoList.backend.ts
@@ -2,10 +2,9 @@ import express, {Request, Response} from 'express'
 import {TodoListItem} from "client/TodoList.model";
 import {fetchCommentStatistics, fetchTodos} from "./internal-api";
 
-const router = express.Router()
-export default router
+export default express.Router()
 
-router.get('/api/todo-list/', async (req: Request, res: Response<TodoListItem[]>) => {
+.get('/api/todo-list/', async (req: Request, res: Response<TodoListItem[]>) => {
   const todos = await fetchTodos()
   const commentStats = await fetchCommentStatistics(todos.map(todo => todo.id))
   const commentsPerTodo = new Map(commentStats.map(stat => [stat.topic, stat.comment_count]))
